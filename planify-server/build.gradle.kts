ee574ec6c3183d2122a67a5620419707137e385e
Alexandru Sirbu <Alexandru.Sirbu@cloudflight.io>
2024-01-13 13:53:36 +0200

    Initiall project setup and architecture

---
+++ planify-server/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.2")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,5 @@
 rootProject.name = "planify"
-
+include("planify-api")
+include("planify-entity-api")
+include("planify-server")
+include("planify-ui")
